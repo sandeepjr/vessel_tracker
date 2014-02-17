a4d4b0103e2da5a9f4b5531ee53aa438764a0929
sandeep.ramachandran <sandeep.ramachandran@bs-shipmanagement.com>
2014-02-17 10:09:24 +0530

    removed unwanted files

--- js/www/js/code.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('#examples').on('change', function(e, ui) {
-		// hackish way
-		setTimeout(function(){window.location.href = e.currentTarget.value;},100)
-	});
-});
